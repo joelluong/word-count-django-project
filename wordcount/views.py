97252de19a2abd9ac47647d1bfce8e459a90be2f
duongluong <101051766@student.swin.edu.au>
2018-10-29 09:06:44 +1100

    Initial Commit

---
+++ wordcount/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+def homepage(request):
+    return render(request, 'home.html')
+
+def eggs(request):
+    return HttpResponse('<h1>Eggs</h1>')
+
+def count(request):
+    fullText = request.GET['fullText']
+
+    wordList = fullText.split()
+
+    wordDictionary = {}
+
+    for word in wordList:
+        if word in wordDictionary:
+            #Increase
+            wordDictionary[word] += 1
+        else:
+            wordDictionary[word] = 1
+
+    sortedWords = sorted(wordDictionary.items(), key=operator.itemgetter(1), reverse = True)
+
+    return render(request, 'count.html', {'fullText':fullText, 'count': len(wordList), 'sortedWords': sortedWords})
+
+def about(request):
+    return render(request, 'about.html')
